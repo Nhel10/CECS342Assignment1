5e6c2ad81d46f7ecda4091f3eaa350d2c3858854
Rios Jairo <dmg138380@yahoo.com>
2021-01-29 02:03:27 +0000

    need to work on the pivot

--- quicksort.c
+++ quicksort.c
@@ -15,17 +15,47 @@ int main(void)
 {
   int a[N] = {4,65,2,-31,0,99,2,83,287,1};
    printf("Hello World!");
+   qsort2(a, N);
+   print(a);
    return 0;
 }
 
 void quick_sort( int * a, int left, int right) // left side of subarray right side of subarray 
 {
-  //int c = 7;
-  int pivot_index = pivot(a, left, right, right/2);
+  
+  int c = 3;
+  if ( (right|| left) <= c)
+    printf("\nHello darkenss my old friend\n");
+  else
+  {
+    int pivot_index = pivot(a, left, right, right/2);
 
-  quick_sort(a, left, pivot_index -1 ); // break array to first element up to pivot index
-  quick_sort(a,pivot_index + 1, right); // second part of subarray starting from ppivot to last index
+    quick_sort(a, left, pivot_index -1 ); // break array to first element up to pivot index
+    quick_sort(a,pivot_index + 1, right); // second part of subarray starting from ppivot to last index
+  }
 
+}
+
+int pivot(int *a, int left, int right, int center)
+{
+  if(a[left] > a[right])
+  {
+    if(a[right] > a[center])
+      return right;
+    else if(a[left] > a[center])
+      return center;
+    else 
+      return left;
+  }
+  else
+  {
+    if(a[left] > a[center])
+      return left;
+    else if(a[right] > a[center])
+      return center;
+    else
+      return right;
+  }
 }
 
 void print(int *a)
@@ -34,7 +64,7 @@ void print(int *a)
     printf(" %d",a[i]);
 }
 
-int partition( int * a, int left, int right, int pivot_index )
+int partition( int * a, int left, int right, int pivot_index)
 {
   int pivot_value = a[pivot_index];
   swapf(&a[pivot_index],&a[right]);
@@ -54,12 +84,12 @@ int partition( int * a, int left, int right, int pivot_index )
   return store;
 }
 
-void qsort( int *a, int n) //quick sort array a with n elements
+void qsort2( int *a, int n) //quick sort array a with n elements
 {
   quick_sort(a, 0, n);
 }
 
-void swatf(int * a, int *b)//swap value function 
+void swapf(int * a, int *b)//swap value function 
 {
   int temp = *a;
   *a = *b;
